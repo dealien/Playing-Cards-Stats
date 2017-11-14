9db5d0f4403b0b6cb00f672b762fce992f690621
Unknown <dragonofawsome@gmail.com>
2017-11-14 11:50:41 -0500

    Fixed generator

--- script.js
+++ script.js
@@ -1,26 +1,67 @@
 function genCards() {
+    if ($('#perm').is(':checked')) {
+        var type = true;
+        $('#count').attr('max', 10000);
+    } else {
+        var type = false;
+        $('#count').attr('max', 20);
+    }
     var count = parseInt($("#count").val());
-    if ($('#perm').is(':checked')) { var type = true; } else { var type = false; }
-    console.log(type);
+    if (type == false && count > 20) {
+        count = 20;
+        $("#count").val(20);
+    } else if (count < 1) {
+        count = 1;
+        $("#count").val(1);
+    } else if (count > 20000) {
+        count = 20000;
+        $("#count").val(20000);
+    }
+    // console.log('type', type);
 
     var ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
     var suits = ['diams', 'hearts', 'spades', 'clubs'];
-    var card;
     var hand = '';
-    var test = '';
-
+    var repeats = 0;
     for (var i = 0; i < count; i++) {
-        var rank = ranks[Math.floor(Math.random() * ranks.length)];
-        var suit = suits[Math.floor(Math.random() * suits.length)];
+        if (type == true) {
+            var rank = ranks[Math.floor(Math.random() * ranks.length)];
+            var suit = suits[Math.floor(Math.random() * suits.length)];
 
-        var t = rank + ',' + suit + '\n';
-        test += t;
+            var t = rank + ',' + suit + '\n';
+            // console.log(t);
 
-        card = '<li><span class="card rank-' + rank.toString().toLowerCase() + ' ' + suit + '"><span class="rank">' + rank + '</span><span class="suit">&' + suit + ';</span></span></li>\n';
+            var card = '<li><span class="card rank-' + rank.toString().toLowerCase() + ' ' + suit + '"><span class="rank">' + rank + '</span><span class="suit">&' + suit + ';</span></span></li>\n';
+            // console.log(card);
+        } else {
+            var card;
+            card = genCard();
+            while (hand.indexOf(card) >= 0) {
+                card = genCard();
+                repeats += 1;
+            }
+        }
         // console.log(card);
         hand += card;
+        $(".table").html(hand);
+        if (type == false) {
+            $('.result').html('Result: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;(Repeats: ' + repeats + ')');
+            console.log(repeats);
+        }
     }
+    return repeats;
+}
 
-    $(".table").html(hand);
-    console.log(test);
+function genCard() {
+    var ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
+    var suits = ['diams', 'hearts', 'spades', 'clubs'];
+
+    var rank = ranks[Math.floor(Math.random() * ranks.length)];
+    var suit = suits[Math.floor(Math.random() * suits.length)];
+
+    var t = rank + ',' + suit + '\n';
+    // console.log(t);
+
+    card = '<li><span class="card rank-' + rank.toString().toLowerCase() + ' ' + suit + '"><span class="rank">' + rank + '</span><span class="suit">&' + suit + ';</span></span></li>\n';
+    return card;
 }
